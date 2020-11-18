b084f5922fb068d85433a08120c22e233cb0ca6c
vizireanuandrei-cristian <vizireanuandreicristian10@gmail.com>
2020-11-18 23:02:53 +0200

    Nothing Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("ceva");
+        System.out.println("cevaddsddsds");
 
+
+        int i = 0;
     }
 }
